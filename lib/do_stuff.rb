350f8bda43f081e3ce28605c9f882b3f670b6250
Jeff Moore <pair+jeff@mavenlink.com>
2013-09-20 16:07:55 -0700

    asdasdasd

--- lib/do_stuff.rb
+++ lib/do_stuff.rb
@@ -1,31 +1,32 @@
 module DoStuff
   def move_around
-    if @times_moved_vertically >= 3
-      @times_moved_vertically = 0
-      if can_move? move!(EAST)
-        @times_moved_horizontally += 1
-        move!(EAST)
-      else
-        @times_moved_horizontally += 1
-        move!(WEST)
-      end
-    elsif @times_moved_horizontally >= 3
-      @times_moved_horizontally = 0
-      if can_move? move!(NORTH)
-        @times_moved_vertically += 1
-        move!(NORTH)
-      else
-        @times_moved_vertically += 1
-        move!(SOUTH)
-      end
-    else
-      if can_move? move!(NORTH)
-        @times_moved_vertically += 1
-        move!(NORTH)
-      else
-        @times_moved_vertically += 1
-        move!(SOUTH)
-      end
-    end
+    first_possible_move('nswe')
+    #if @times_moved_vertically >= 3
+    #  @times_moved_vertically = 0
+    #  if can_move? move!(EAST)
+    #    @times_moved_horizontally += 1
+    #    move!(EAST)
+    #  else
+    #    @times_moved_horizontally += 1
+    #    move!(WEST)
+    #  end
+    #elsif @times_moved_horizontally >= 3
+    #  @times_moved_horizontally = 0
+    #  if can_move? move!(NORTH)
+    #    @times_moved_vertically += 1
+    #    move!(NORTH)
+    #  else
+    #    @times_moved_vertically += 1
+    #    move!(SOUTH)
+    #  end
+    #else
+    #  if can_move? move!(NORTH)
+    #    @times_moved_vertically += 1
+    #    move!(NORTH)
+    #  else
+    #    @times_moved_vertically += 1
+    #    move!(SOUTH)
+    #  end
+    #end
   end
 end
